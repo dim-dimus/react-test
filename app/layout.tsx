5831d4e159aa4bf8c86d97d8c5267093a670c544
Dmitry Od <dmitry.odulo@mainsoft.org>
2026-05-08 21:07:14 +0200

    reinit

---
+++ app/layout.tsx
@@ -0,0 +1,63 @@
+import type { Metadata } from "next";
+import { Schibsted_Grotesk, Martian_Mono, Geist } from "next/font/google";
+import "./globals.css";
+import { cn } from "@/lib/utils";
+import LightRays from "@/components/LightRays";
+import {aborted} from "node:util";
+import Navbar from "@/components/Navbar";
+
+const geist = Geist({subsets:['latin'],variable:'--font-sans'});
+
+const schibstedGrotesk = Schibsted_Grotesk({
+  variable: "--font-schibsted-grotesk-sans",
+  subsets: ["latin"],
+});
+
+const martianMono = Martian_Mono({
+  variable: "--font-matrian-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "DevEvent",
+  description: "The Hub For Developers",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html
+      lang="en"
+      className={cn("h-full", "min-h-screen", "antialiased", schibstedGrotesk.variable, martianMono.variable, "font-sans", geist.variable)}
+    >
+      <body className="min-h-full flex flex-col">
+
+        <Navbar />
+
+        <div className="absolute inset-0 top-0 z-[-1] min-h-screen">
+          <LightRays
+              raysOrigin="top-center-offset"
+              raysColor="#5dfeca"
+              raysSpeed={1}
+              lightSpread={0.5}
+              rayLength={3}
+              followMouse={true}
+              mouseInfluence={0.1}
+              noiseAmount={0}
+              distortion={0}
+              className="custom-rays"
+              pulsating={false}
+              fadeDistance={1}
+              saturation={1}
+          />
+        </div>
+        <main>
+          {children}
+        </main>
+      </body>
+    </html>
+  );
+}
